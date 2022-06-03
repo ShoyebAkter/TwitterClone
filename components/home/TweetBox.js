db4ed04e49799e65cf207a6117256b3b46f3f880
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-06-03 19:29:36 +0600

    icons added

--- components/home/TweetBox.js
+++ components/home/TweetBox.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { BsCardImage, BsEmojiSmile } from 'react-icons/bs'
+import { RiFileGifLine, RiBarChartHorizontalFill } from 'react-icons/ri'
+import { IoMdCalendar } from 'react-icons/io'
+import { MdOutlineLocationOn } from 'react-icons/md'
 
 
 const style = {
@@ -13,36 +17,48 @@ const style = {
     submitGeneral: `px-6 py-2 rounded-3xl font-bold`,
     inactiveSubmit: `bg-[#196195] text-[#95999e]`,
     activeSubmit: `bg-[#1d9bf0] text-white`,
-  }
+}
 
 const TweetBox = () => {
-    const [tweetMessage,setTweetMessage]=useState('')
+    const [tweetMessage, setTweetMessage] = useState('')
 
-    console.log(tweetMessage)
+    const postTweet = (event) => {
+        event.preventDefault();
+        console.log(tweetMessage)
+    }
+
     return (
         <div className={style.wrapper}>
             <div className={style.tweetBoxLeft}>
                 <img
-                src="https://media-exp1.licdn.com/dms/image/C5603AQGvm80VWkz21w/profile-displayphoto-shrink_800_800/0/1641269680921?e=1659571200&v=beta&t=yhSFupR1mHO3l0g_5HcA3mk3dupxQSX7w9XdncmZYnQ"
-                alt="profile image"
-                className={style.profileImage}
+                    src="https://media-exp1.licdn.com/dms/image/C5603AQGvm80VWkz21w/profile-displayphoto-shrink_800_800/0/1641269680921?e=1659571200&v=beta&t=yhSFupR1mHO3l0g_5HcA3mk3dupxQSX7w9XdncmZYnQ"
+                    alt="profile image"
+                    className={style.profileImage}
                 />
             </div>
             <div className={style.tweetBoxRight}>
                 <form>
                     <textarea
-                    className={style.inputField}
-                    placeholder="What's happening?"
-                    value={tweetMessage}
-                    onChange={(e)=>setTweetMessage(e.target.value)}
+                        className={style.inputField}
+                        placeholder="What's happening?"
+                        value={tweetMessage}
+                        onChange={(e) => setTweetMessage(e.target.value)}
                     />
                     <div className={style.formLowerContainer}>
-                        <div className={style.iconsContainer}></div>
+                        <div className={style.iconsContainer}>
+                            <BsCardImage className={style.icon} />
+                            <RiFileGifLine className={style.icon} />
+                            <RiBarChartHorizontalFill className={style.icon} />
+                            <BsEmojiSmile className={style.icon} />
+                            <IoMdCalendar className={style.icon} />
+                            <MdOutlineLocationOn className={style.icon} />
+                        </div>
                         <button
-                        type='submit'
-                        className={`${style.submitGeneral} ${
-                            tweetMessage ? style.activeSubmit : style.inactiveSubmit
-                        }`}
+                            type='submit'
+                            disabled={!tweetMessage}
+                            onClick={(event) => postTweet(event)}
+                            className={`${style.submitGeneral} ${tweetMessage ? style.activeSubmit : style.inactiveSubmit
+                                }`}
                         >Tweet</button>
                     </div>
                 </form>
