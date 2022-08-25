27e22dbaf96332f5f52b2f160efe630171cc7098
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-08-25 22:22:18 +0600

    metamask authentication added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useContext } from 'react'
+import { TwitterContext } from '../context/TwitterContext'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/home/Feed'
 import Widgets from '../components/Widgets'
@@ -9,14 +11,73 @@ const style = {
   loginContent: `text-3xl font-bold text-center mt-24`,
 }
 
+
 export default function Home() {
+  const { appStatus, connectWallet } = useContext(TwitterContext)
+
+  const app = (status = appStatus) => {
+    switch (status) {
+      case 'connected':
+        return userLoggedIn
+
+      case 'notConnected':
+        return noUserFound
+
+      case 'noMetaMask':
+        return noMetaMaskFound
+
+      case 'error':
+        return error
+
+      default:
+        return loading
+    }
+  }
+  const userLoggedIn = (
+    < div className={style.content} >
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div >
+  )
+
+  const noUserFound = (
+    <div className={style.loginContainer}>
+      {/* <Image src={metamaskLogo} width={200} height={200} /> */}
+      <div
+        className={style.walletConnectButton}
+        onClick={() => connectWallet()}
+      >
+        Connect Wallet
+      </div>
+      <div className={style.loginContent}>Connect to Metamask.</div>
+    </div>
+  )
+
+  const noMetaMaskFound = (
+    <div>
+      <p>No meta mask found</p>
+    </div>
+  )
+
+  const error = (
+    <div className={style.loginContainer}>
+      {/* <Image src={errorImg} width={250} height={200} /> */}
+      <div className={style.loginContent}>
+        An error occurred. Please try again later or from another browser.
+      </div>
+    </div>
+  )
+
+  const loading = (
+    <div className={style.loginContainer}>
+      <div className={style.loginContent}>Loading...</div>
+    </div>
+  )
+
   return (
     <div className={style.wrapper}>
-      <div className={style.content}>
-      <Sidebar/>
-      <Feed/>
-      <Widgets/>
-      </div>
+      {app(appStatus)}
     </div>
   )
 }
